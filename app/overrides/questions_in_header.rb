97fd116433b451c86842e0db610d0195e567932e
Tamas Drahos <drahostamas@gmail.com>
2012-10-11 19:20:55 +0200

    menu current class

--- app/overrides/questions_in_header.rb
+++ app/overrides/questions_in_header.rb
@@ -1,4 +1,4 @@
 Deface::Override.new(:virtual_path => "spree/layouts/spree_application",
                      :name => "questions_in_header",
                      :insert_bottom => "#main-nav-bar",
-                     :text => "<li><%= link_to( t('questions'), questions_path) %></li>")
+                     :text => "<li class=\"<%= 'current' if request.path == questions_path %>\"><%= link_to( t('questions'), questions_path) %></li>")
